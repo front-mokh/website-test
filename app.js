3907c9e4f498ba72a485d384825f80c21d90a519
front-mokh <142145455+front-mokh@users.noreply.github.com>
2023-08-15 08:28:14 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+const themeIcon = document.getElementById('theme-icon');
+const phoneIcon = document.getElementById('phone-icon');
+const mailIcon = document.getElementById('mail-icon');
+const locationIcon = document.getElementById('location-icon');
+
+themeIcon.onclick = function(){
+    document.body.classList.toggle('dark-theme');
+    
+    if(document.body.classList.contains('dark-theme')){
+        themeIcon.src = 'assets/icons/sun.png';
+        phoneIcon.src = 'assets/icons/phone-dark.svg';
+        mailIcon.src = 'assets/icons/mail-dark.svg';
+        locationIcon.src = 'assets/icons/location-dark.svg';
+    }else{
+        themeIcon.src = 'assets/icons/moon.png';
+        phoneIcon.src = 'assets/icons/phone.svg';
+        mailIcon.src = 'assets/icons/mail.svg';
+        locationIcon.src = 'assets/icons/location.svg';
+    }
+}
+
